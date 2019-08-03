902eecbb947da6e3a8a5dc300b1382e57274cbc8
Fernando Fabricio dos Santos <ferfabricio@gmail.com>
2019-08-03 07:01:21 -0300

    v1.0.0

---
+++ precache-manifest.a2cb0eac332bffbb69bcff4069a91034.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf9c80e82aee9d9f59584cf82d599f0",
+    "url": "/conference-scan/index.html"
+  },
+  {
+    "revision": "ff47ea0053bc9da7ff8a",
+    "url": "/conference-scan/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f8cd460fe8094d2120a3",
+    "url": "/conference-scan/static/js/2.b3267de1.chunk.js"
+  },
+  {
+    "revision": "ff47ea0053bc9da7ff8a",
+    "url": "/conference-scan/static/js/main.492157f5.chunk.js"
+  },
+  {
+    "revision": "396acdeb5e7f0b8e4d49",
+    "url": "/conference-scan/static/js/runtime~main.628d4c3d.js"
+  }
+]);
